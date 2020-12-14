4b3ae9076bc3fa239f66968e9fa22352e76d5cbb
elanj <elans3.java@gmail.com>
2020-12-14 16:55:43 -0600

    Idaithalam contract testing  demo code

--- src/test/java/io/virtualan/test/ContractValidationTestExecution.java
+++ src/test/java/io/virtualan/test/ContractValidationTestExecution.java
@@ -6,11 +6,11 @@ import org.junit.Test;
 
 public class ContractValidationTestExecution {
     @Test
-    public static void main(String[] args) {
+    public static void ContractValidationTest() {
         //Initiate the contract testing
         //Generate feature file from POSTMAN Collection
         //Execute and Generate the HTML Cucumber report
-        IdaithalamExecutor.main( new String[]{"Pet API Production Checkout"});
+        IdaithalamExecutor.validateContract("Pet API Production Checkout");
         Assert.assertTrue(true);
     }
 }
