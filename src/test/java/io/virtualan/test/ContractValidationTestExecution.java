9c307ac16ed8bb55c775c0413f428c91995d750c
Elan Thangamani <elans3.java@gmail.com>
2020-12-17 22:24:36 -0600

    Updated with 1.0.1 version

--- src/test/java/io/virtualan/test/ContractValidationTestExecution.java
+++ src/test/java/io/virtualan/test/ContractValidationTestExecution.java
@@ -10,7 +10,18 @@ public class ContractValidationTestExecution {
         //Initiate the contract testing
         //Generate feature file from POSTMAN Collection
         //Execute and Generate the HTML Cucumber report
-        IdaithalamExecutor.validateContract("Pet API Production Checkout");
-        Assert.assertTrue(true);
+        int status =0 ;
+        try {
+            status = IdaithalamExecutor.validateContract("Pet API Production Checkout");
+            System.out.println(status);
+            if(status != 0) {
+                Assert.assertTrue(false);
+            }
+            Assert.assertTrue   (true);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            Assert.assertTrue(false);
+        }
+
     }
 }
