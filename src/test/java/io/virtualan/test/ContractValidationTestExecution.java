315a40067c27be29306de284a9e9abaf78522059
Elan Thangamani <elans3.java@gmail.com>
2021-06-13 22:57:07 -0500

    Updated with YML based version

--- src/test/java/io/virtualan/test/ContractValidationTestExecution.java
+++ src/test/java/io/virtualan/test/ContractValidationTestExecution.java
@@ -1,25 +1,21 @@
 package io.virtualan.test;
 
-import io.virtualan.idaithalam.contract.IdaithalamExecutor;
+import io.virtualan.idaithalam.core.api.VirtualanTestPlanExecutor;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class ContractValidationTestExecution {
+
     @Test
     public void testContractValidation() {
-        //Initiate the contract testing
-        //Generate feature file from POSTMAN Collection
-        //Execute and Generate the HTML Cucumber report
-        int status =0 ;
         try {
-            status = IdaithalamExecutor.validateContract("Scriptless API Testing / NO-OR-Low code Automation Framework");
-            System.out.println(status);
-            if(status != 0) {
+            boolean isSuccess = VirtualanTestPlanExecutor.invoke("apiexecution.yaml");
+            if (!isSuccess) {
                 Assert.assertTrue(false);
             }
-            Assert.assertTrue   (true);
-        }catch (Exception e){
-            System.out.println(e.getMessage());
+            Assert.assertTrue(true);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
             Assert.assertTrue(false);
         }
 
